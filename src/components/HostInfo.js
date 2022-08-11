a795dcc87b0fdabe84e0d9fdc36db20a7e1c347b
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-11 16:49:54 -0500

    Cleanup

--- src/components/HostInfo.js
+++ src/components/HostInfo.js
@@ -36,12 +36,6 @@ function HostInfo({ areas, selectedHostId, updateHost }) {
       )
     })
   }
-  // const [options] = useState([
-  //   { key: "some_area", text: "Some Area", value: "some_area" },
-  //   { key: "another_area", text: "Another Area", value: "another_area" },
-  // ]);
-
-  // const [value] = useState("some_area");
 
   function handleOptionChange(e, { value }) {
     updateHost({ ...host, area: value });
@@ -68,11 +62,8 @@ function HostInfo({ areas, selectedHostId, updateHost }) {
           <Card.Content>
             <Card.Header>
               { host.name } | {host.gender === 'Male' ? <Icon name="man" /> : <Icon name="woman" />}
-              {/* Think about how the above should work to conditionally render the right First Name and the right gender Icon */}
             </Card.Header>
             <Card.Meta>
-              {/* Sometimes the label should take "Decommissioned". How are we going to conditionally render that? */}
-              {/* Checked takes a boolean and determines what position the switch is in. Should it always be true? */}
               <Radio
                 onChange={ handleRadioChange }
                 label={ isActive ? "Active" : "Decommissioned" }
@@ -96,68 +87,3 @@ function HostInfo({ areas, selectedHostId, updateHost }) {
 }
 
 export default HostInfo;
-
-// function HostInfo({ selectedHostId }) {
-//   // This state is just to show how the dropdown component works.
-//   // Options have to be formatted in this way (array of objects with keys of: key, text, value)
-//   // Value has to match the value in the object to render the right text.
-
-//   // IMPORTANT: But whether it should be stateful or not is entirely up to you. Change this component however you like.
-//   const [options] = useState([
-//     { key: "some_area", text: "Some Area", value: "some_area" },
-//     { key: "another_area", text: "Another Area", value: "another_area" },
-//   ]);
-
-//   const [value] = useState("some_area");
-
-//   function handleOptionChange(e, { value }) {
-//     // the 'value' attribute is given via Semantic's Dropdown component.
-//     // Put a debugger or console.log in here and see what the "value" variable is when you pass in different options.
-//     // See the Semantic docs for more info: https://react.semantic-ui.com/modules/dropdown/#usage-controlled
-//   }
-
-//   function handleRadioChange() {
-//     console.log("The radio button fired");
-//   }
-
-//   return (
-//     <Grid>
-//       <Grid.Column width={6}>
-//         <Image
-//           src={/* pass in the right image here */ ""}
-//           floated="left"
-//           size="small"
-//           className="hostImg"
-//         />
-//       </Grid.Column>
-//       <Grid.Column width={10}>
-//         <Card>
-//           <Card.Content>
-//             <Card.Header>
-//               {"Bob"} | {true ? <Icon name="man" /> : <Icon name="woman" />}
-//               {/* Think about how the above should work to conditionally render the right First Name and the right gender Icon */}
-//             </Card.Header>
-//             <Card.Meta>
-//               {/* Sometimes the label should take "Decommissioned". How are we going to conditionally render that? */}
-//               {/* Checked takes a boolean and determines what position the switch is in. Should it always be true? */}
-//               <Radio
-//                 onChange={handleRadioChange}
-//                 label={"Active"}
-//                 checked={true}
-//                 slider
-//               />
-//             </Card.Meta>
-//             <Divider />
-//             Current Area:
-//             <Dropdown
-//               onChange={handleOptionChange}
-//               value={value}
-//               options={options}
-//               selection
-//             />
-//           </Card.Content>
-//         </Card>
-//       </Grid.Column>
-//     </Grid>
-//   );
-// }
